c17396f0edd84d3c6580d19ac8d257b81676573c
ziedlahbib <lahbibmedzied@gmail.com>
2023-01-01 23:15:06 +0100

    navbar sidenav and footer back office ok

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+   w3_open() {
+    (<HTMLInputElement>document.getElementById("main")).style.marginLeft = "14%";
+    (<HTMLInputElement>document.getElementById("mySidebar")).style.width = "25%";
+  (<HTMLInputElement>document.getElementById("mySidebar")).style.display = "block";
+  (<HTMLInputElement>document.getElementById("openNav")).style.display = 'none';
+  (<HTMLInputElement>document.getElementById("openNav")).style.height = '57px';
+  (<HTMLInputElement>document.getElementById("closeNav")).style.display = "inline-block";
 
+
+  
+  }
+  
+   w3_close() {
+    (<HTMLInputElement>document.getElementById("main")).style.marginLeft = "0%";
+    (<HTMLInputElement>document.getElementById("mySidebar")).style.display = "none";
+   (<HTMLInputElement>document.getElementById("openNav")).style.display = "inline-block";
+   (<HTMLInputElement>document.getElementById("closeNav")).style.display = "none";
+  
+  }
 }
